18a104d60a7695b5d95ed94de4092981a430a96c
navedqb <109583873+navedqb@users.noreply.github.com>
2024-07-30 15:44:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from datetime import datetime
 
 now = datetime.now()
+version = pkg_resources.get_distribution("virtualenv").version
 
 print("Current date and time:", now.strftime("%Y-%m-%d %H:%M:%S"))
+print("VirtualEnv version:", version)
