ef367e281ccec47cfe3269056ab69f8d927e0879
navedqb <109583873+navedqb@users.noreply.github.com>
2024-07-30 16:05:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from datetime import datetime
+import importlib.metadata
 
 now = datetime.now()
+version = importlib.metadata.version("virtualenv")
+
 
 print("Current date and time:", now.strftime("%Y-%m-%d %H:%M:%S"))
+print("VirtualEnv version:", version)
